2aa78db6b675413ceef768649c1f1b618ad286f7
Nogunaa <nogunaa@gmail.com>
2023-02-02 11:06:05 -0700

    The completion of resume, projects and contact pgs

---
+++ script.js
@@ -0,0 +1,5 @@
+const swup = new Swup()
+
+document.getElementById("dropdown-icon").addEventListener("click", function(){
+    document.getElementsByClassName("dropdown-content").classList.add("toggle-open");
+});
